78c9523ddc107278d79f034ee393081f5f9cae58
fefe4 <mynameisfefe@protonmail.com>
2021-05-23 21:41:48 -0300

    set basic navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import menu2 from './assets/images/menu2.png'
 
 function App() {
   return (
-   <div>
-    <h1>"holo"</h1>
-    <h2>"hola"</h2>
-    <h3>"hole"</h3>
-   </div>
+    
+   <nav>
+     <ul className="container d-flex list-unstyled justify-content-between align-items-center">
+       <li className="d-flex align-items-center">
+         <a><img src={menu2} alt="menu" /></a>
+         <a className="m-auto">fefe's blog</a>
+       </li>
+       <li ><a  href="#aslas"> Log-in & user profile pic</a></li>
+     </ul>
+   </nav>
   );
 }
 
