02fba4f875550c93a252e92fc1c64c2872e1448d
fefe4 <mynameisfefe@protonmail.com>
2021-06-11 00:06:58 -0300

    edit blog works

--- src/components/blog.js
+++ src/components/blog.js
@@ -2,58 +2,100 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Blog = ({ match }) => {
-  
+  //edit values
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [description, setDescription] = useState("");
+
+  const updateTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const updateBody = (e) => {
+    setBody(e.target.value);
+  };
+  const updateDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
   useEffect(() => {
     fetchBlog();
   }, []);
   const [blog, setBlog] = useState({});
-  const [hasEditBeenPressed, setHasEditBeenPressed] = useState (true)
+  const [hasEditBeenPressed, setHasEditBeenPressed] = useState(false);
+
+  const activateEditMode = () => {
+    hasEditBeenPressed? setHasEditBeenPressed(false) : setHasEditBeenPressed(true)
+  }
+
+
   const fetchBlog = async () => {
-    const data = await fetch(`http://localhost:4000/api/blogs/${match.params.id}`);
+    const data = await fetch(
+      `http://localhost:4000/api/blogs/${match.params.id}`
+    );
     const blogApi = await data.json();
     const blog = blogApi.blog_;
     setBlog(blog);
+    setDescription(blog.description);
+    setTitle(blog.title);
+    setBody(blog.body);
   };
 
-const actionForm = `http://localhost:4000/api/blogs/${match.params.id}`
+  const actionForm = `http://localhost:4000/api/blogs/${match.params.id}`;
 
- //put requests don't work for windows.
- const editBlog = (e) => {
-  e.preventDefault();
-  // PUT request using fetch with async/await
-  async function updatePost() {
+  //put requests don't work for windows.
+  const editBlog = (e) => {
+    e.preventDefault();
+    // PUT request using fetch with async/await
+    async function updatePost() {
       const requestOptions = {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ title: 'React Hooks PUT Request Example', body:"lalalala", description:"loloolo"})
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: title,
+          body: body,
+          description: description,
+        }),
       };
       const response = await fetch(`${actionForm}`, requestOptions);
       const data = await response.json();
-      console.log(data)
-  }
-  updatePost();
-}
+      console.log(data);
+    }
+    updatePost();
+  };
 
-
-  
   return (
     <main className="">
-      {hasEditBeenPressed 
-      ? <form onSubmit={editBlog}>
+      <button onClick={activateEditMode}>Edit</button>
+      {hasEditBeenPressed ? (
+        <form onSubmit={editBlog}>
           <label for="title">Title</label>
-          <input name="title" type="text" placeholder={blog.title}/>
+          <input
+            onChange={updateTitle}
+            name="title"
+            type="text"
+            value={title}
+          />
           <label for="description">Description</label>
-          <input name="description" placeholder={blog.description} type="text" />
+          <input
+            onChange={updateDescription}
+            name="description"
+            value={description}
+            type="text"
+          />
           <label for="body">Body</label>
-          <input name="body" type="text" placeholder={blog.body}/>
+          <input onChange={updateBody} name="body" type="text" value={body} />
           <button>Submit</button>
         </form>
-      : <div>
-          <h4> {blog.title} <span>{blog.date}</span></h4>
+      ) : (
+        <div>
+          <h4>
+            {" "}
+            {blog.title} <span>{blog.date}</span>
+          </h4>
           <h5>{blog.author}</h5>
           <p>{blog.body}</p>
-        </div>  
-      }
+        </div>
+      )}
     </main>
   );
 };
